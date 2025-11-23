9f6b73fee2d9544f3969cbb3635aad7f1631cd24
Piper Inns Hall <piper.innshall@gmail.com>
2025-11-23 23:04:55 +1300

    chore: Output ppm image

--- src/image.rs
+++ src/image.rs
@@ -3,5 +3,5 @@ pub struct Color {
     pub r: u8,
     pub g: u8,
     pub b: u8,
-    pub a: u8
+    pub a: u8,
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,24 @@ pub mod image;
 use image::Color;
 
 fn main() {
-    println!("\n --- Begin Raytracing ---");
+    let image_width = 256;
+    let image_height = 256;
+
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for i in 0..image_height {
+        for j in 0..image_width {
+            let r = j as f64 / (image_width - 1) as f64;
+            let g = i as f64 / (image_height - 1) as f64;
+            let b = 0.0;
+            let color = Color {
+                r: (255.999 * r) as u8,
+                g: (255.999 * g) as u8,
+                b: (255.999 * b) as u8,
+                a: 255,
+            };
+            
+            println!("{} {} {}", color.r, color.g, color.b);
+        }
+    }
 }
