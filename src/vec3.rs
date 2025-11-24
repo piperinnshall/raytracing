1b684b0bb2123a580345cd5a61e280c106f9fccc
Piper Inns Hall <piper.innshall@gmail.com>
2025-11-25 00:53:04 +1300

    chore: Complete all operator implementations for Vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -6,89 +6,88 @@ pub struct Vec3 {
 }
 
 impl Vec3 {
-    pub fn new(x: f64, y: f64, z: f64) -> Vec3 {
-        Vec3 { e: [x, y, z] }
-    }
-    pub fn x(&self) -> f64 {
-        self.e[0]
-    }
-    pub fn y(&self) -> f64 {
-        self.e[1]
-    }
-    pub fn z(&self) -> f64 {
-        self.e[2]
-    }
+    pub fn new(x: f64, y: f64, z: f64) -> Vec3 { Vec3 { e: [x, y, z] } }
+    pub fn x(&self) -> f64 { self[0] }
+    pub fn y(&self) -> f64 { self[1] }
+    pub fn z(&self) -> f64 { self[2] }
 }
 
 pub type Point3 = Vec3;
 
-// -Vec3
-impl Neg for Vec3 {
+macro_rules! impl_op {
+    ($trait:ident, $func:ident, $op:tt) => {
+        impl $trait for Vec3 {
+            type Output = Vec3;
+
+            fn $func(self, rhs: Self) -> Self::Output {
+                Self::new(
+                    self[0] $op rhs[0],
+                    self[1] $op rhs[1],
+                    self[2] $op rhs[2],
+                )
+            }
+        }
+    };
+}
+
+impl_op!(Sub, sub, -); // Vec3 - Vec3
+impl_op!(Add, add, +); // Vec3 + Vec3
+impl_op!(Mul, mul, *); // Vec3 * Vec3
+
+impl Mul<f64> for Vec3 { // Vec3 * f64
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self::new(self[0] * rhs, self[1] * rhs, self[2] * rhs)
+    }
+}
+
+impl Div<f64> for Vec3 { // Vec3 / f64
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self * (1.0 / rhs)
+    }
+}
+                       
+impl AddAssign for Vec3 { // Vec3 += Vec3
+    fn add_assign(&mut self, rhs: Self) {
+        self[0] += rhs[0];
+        self[1] += rhs[1];
+        self[2] += rhs[2];
+    }
+}
+
+impl MulAssign<f64> for Vec3 { // Vec3 *= f64
+    fn mul_assign(&mut self, rhs: f64) {
+        self[0] *= rhs;
+        self[1] *= rhs;
+        self[2] *= rhs;
+    }
+}
+
+impl DivAssign<f64> for Vec3 { // Vec3 /= f64
+    fn div_assign(&mut self, rhs: f64) {
+        *self *= 1.0 / rhs
+    }
+}
+
+impl Neg for Vec3 { // -Vec3
     type Output = Vec3;
 
     fn neg(self) -> Self::Output {
-        return Self::new(-self.x(), -self.y(), -self.z());
+        return Self::new(-self[0], -self[1], -self[2]);
     }
 }
-// Vec3[i]
-impl Index<usize> for Vec3 {
+impl Index<usize> for Vec3 { // Vec3[i]
     type Output = f64;
 
     fn index(&self, index: usize) -> &Self::Output {
         &self.e[index]
     }
 }
-// Vec3[i] = 0
-impl IndexMut<usize> for Vec3 {
+impl IndexMut<usize> for Vec3 { // Vec3[i] = 0
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.e[index]
     }
 }
-//
-// // Vec3 += Vec3
-// impl AddAssign for Vec3 {
-//     fn add_assign(&mut self, rhs: Self) {
-//         *self = *self + rhs;
-//     }
-// }
-// impl MulAssign for Vec3 {
-//     fn mul_assign(&mut self, rhs: Self) {
-//         *self = *self * rhs;
-//     }
-// }
-
-macro_rules! impl_op {
-    ($trait:ident, $func:ident, $op:tt, Vec3) => {
-        impl $trait for Vec3 {
-            type Output = Vec3;
-
-            fn $func(self, rhs: Self) -> Self::Output {
-                Self::new(
-                    self.e[0] $op rhs.e[0],
-                    self.e[1] $op rhs.e[1],
-                    self.e[2] $op rhs.e[2],
-                )
-            }
-        }
-    };
-    ($trait:ident, $func:ident, $op:tt, $rhs:ty) => {
-        impl $trait<$rhs> for Vec3 {
-            type Output = Vec3;
-
-            fn $func(self, rhs: $rhs) -> Self::Output {
-                Self::new(
-                    self.e[0] $op rhs,
-                    self.e[1] $op rhs,
-                    self.e[2] $op rhs,
-                )
-            }
-        }
-    };
-}
-
-impl_op!(Add, add, +, Vec3); // Vec3 + Vec3
-impl_op!(Sub, sub, -, Vec3); // Vec3 - Vec3
-impl_op!(Mul, mul, *, Vec3); // Vec3 * Vec3
-impl_op!(Mul, mul, *, f64); // Vec3 * f64
-impl_op!(Div, div, /, f64); // Vec3 / f64
-
