ba2e51743db089c66959033104c46ddd27e2fcd2
Piper Inns Hall <piper.innshall@gmail.com>
2025-11-28 23:14:33 +1300

    feat: Completed anti-aliasing

--- src/camera.rs
+++ src/camera.rs
@@ -1,29 +1,30 @@
-use crate::vec3::{Vec3,Point3};
-use crate::hit::{HitRecord, Hittable, HitList};
+use crate::color::{self, Color};
+use crate::hit::{HitList, HitRecord, Hittable};
 use crate::interval::Interval;
 use crate::ray::Ray;
-use crate::color::{self, Color};
+use crate::utils;
+use crate::vec3::{Point3, Vec3};
 use std::io;
 
 pub struct Camera {
-    aspect_ratio: f64,
+    aspect_ratio: f64, // Ratio of image width over height
     focal_length: f64,
-    image_width: i32,
-    image_height: i32,
-    viewport_width: f64,
-    viewport_height: f64,
+    image_width: i32, // Rendered image width in pixel count
+    image_height: i32, // Rendered image height in pixel count
+    viewport_width: f64, // Viewport width in pixel count
+    viewport_height: f64, // Viewport height in pixel count
     viewport_u: Vec3,
     viewport_v: Vec3,
     pixel_delta_u: Vec3,
     pixel_delta_v: Vec3,
-    center: Point3,
-    pixel_00_loc: Point3,
-    samples_per_pixel: i32,
-
+    center: Point3, // Camera center
+    pixel_00_loc: Point3, // Location of pixel 0, 0
+    samples_per_pixel: i32, // Count of random samples  for each pixel
+    pixel_samples_scale: f64 // Color scale factor for a small sum of pixel samples
 }
 
-impl Camera { 
-    pub fn new(aspect_ratio: f64, viewport_height: f64, image_height: i32) -> Self { 
+impl Camera {
+    pub fn new(aspect_ratio: f64, viewport_height: f64, image_height: i32) -> Self {
         let focal_length = 1.0;
 
         let image_width = (image_height as f64 * aspect_ratio) as i32;
@@ -32,19 +33,19 @@ impl Camera {
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
         let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
         let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
-        
+
         // Calculate the horizontal and vertical delta vectors from pixel to pixel.
         let pixel_delta_u = viewport_u / image_width as f64;
         let pixel_delta_v = viewport_v / image_height as f64;
 
         let center = Vec3::default();
-        let pixel_00_loc: Point3 = 
-            Self::upper_left(center, viewport_u, viewport_v, focal_length) 
+        let pixel_00_loc: Point3 = Self::upper_left(center, viewport_u, viewport_v, focal_length)
             + (pixel_delta_u + pixel_delta_v) * 0.5;
 
-        let samples_per_pixel = 10;
+        let samples_per_pixel = 100;
+        let pixel_samples_scale = 1.0 / samples_per_pixel as f64;
 
-        Self { 
+        Self {
             aspect_ratio,
             focal_length,
             image_width,
@@ -58,7 +59,8 @@ impl Camera {
             center,
             pixel_00_loc,
             samples_per_pixel,
-        } 
+            pixel_samples_scale,
+        }
     }
 
     pub fn render(&self, world: &HitList) {
@@ -67,31 +69,50 @@ impl Camera {
         color::write_header(&mut io::stdout(), self.image_width, self.image_height);
 
         for col in 0..self.image_height {
-
-            // eprintln!("scan lines remaining: {}", (self.image_height - col));
+            eprintln!("scan lines remaining: {}", (self.image_height - col));
 
             for row in 0..self.image_width {
+                let mut pixel_color = Color::default();
 
-                let pixel_center = self.pixel_00_loc 
-                    + (self.pixel_delta_u * row as f64) 
-                    + (self.pixel_delta_v * col as f64);
+                for sample in 0..self.samples_per_pixel {
+                    let ray = self.ray(row, col);
+                    pixel_color += Self::color(ray, &world);
+                }
 
-                let ray_direction = pixel_center - self.center;
-                let ray = Ray::new(self.center, ray_direction);
-
-                let color = Self::color(ray, &world);
-
-                color::write_color(&mut io::stdout(), color);
+                color::write_color(&mut io::stdout(), pixel_color * self.pixel_samples_scale );
             }
         }
 
         eprintln!("Done!");
     }
 
+    fn ray(&self, row: i32, col: i32) -> Ray {
+        // Construct a camera ray originating from the origin
+        // and directed at randomly sampled point around the pixel location i, j.
+
+        let offset = self.sample_square();
+        let pixel_sample = self.pixel_00_loc
+            + (self.pixel_delta_u * (offset.x() + row as f64))
+            + (self.pixel_delta_v * (offset.y() + col as f64));
+
+        let ray_direction = pixel_sample - self.center;
+
+        Ray::new(self.center, ray_direction)
+    }
+
+    fn sample_square(&self) -> Vec3 {
+        // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
+        Vec3::new(utils::random_f64() - 0.5, utils::random_f64() - 0.5, 0.0)
+    }
+
+    fn upper_left(center: Point3, u: Vec3, v: Vec3, focal_length: f64) -> Point3 {
+        center - Vec3::new(0.0, 0.0, focal_length) - u / 2.0 - v / 2.0
+    }
+
     fn color(ray: Ray, world: &HitList) -> Color {
         let mut rec = HitRecord::default();
         if world.hit(&ray, Interval::new(0.0, f64::INFINITY), &mut rec) {
-            return (Color::fill(1.0) + rec.normal) * 0.5
+            return (Color::fill(1.0) + rec.normal) * 0.5;
         }
 
         let unit_direction = ray.direction().normalize();
@@ -99,11 +120,5 @@ impl Camera {
 
         color::lerp(Color::fill(1.0), Color::new(0.5, 0.7, 1.0), t)
     }
-
-    fn upper_left(center: Point3, u: Vec3, v: Vec3,focal_length: f64) -> Point3 {
-        center 
-            - Vec3::new(0.0, 0.0, focal_length) 
-            - u / 2.0
-            - v / 2.0
-    }
 }
+
